1638d9aab50c1e49dd814edfb38f669f7d640936
frhl <frederik.heymann@gmail.com>
2024-07-05 11:11:39 +0100

    remove zerod dosage as we are now scaling back to 0-2

--- 02_step2_SPAtests_variant_and_gene.sh
+++ 02_step2_SPAtests_variant_and_gene.sh
@@ -280,10 +280,7 @@ cmd="step2_SPAtests.R \
         --is_output_markerList_in_groupTest=TRUE \
         --is_single_in_groupTest=TRUE \
         --maxMAF_in_groupTest=0.5 \
-        --SAIGEOutputFile=${HOME}/${OUT}.txt \
-        --dosage_zerod_MAC_cutoff=0 \
-        --dosage_zerod_cutoff=0 \
-        --dosage_zerod_MAC_cutoff=0
+        --SAIGEOutputFile=${HOME}/${OUT}.txt
     "
 
 run_container
